9f343a246836ba63549f40340a848475c07bedeb
lepnio <lepnio@gmail.com>
2014-06-14 08:22:35 +0200

    displaying users

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the rabbitmqDemoFrontApp
  */
 angular.module('rabbitmqDemoFrontApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+    var httpPolling = function () {
+      $timeout(function () {
+        $http({method: 'GET', url: 'http://localhost:8080/rabbitmq/users'}).
+          success(function (data) {
+            $scope.users = data;
+          }).
+          error(function () {
+            $scope.error = 'Server is down';
+          });
+        httpPolling();
+      }, 2000);
+    };
+    httpPolling();
   });
