f546f3183a705e7e0a151548e34ad4c950a77e59
lepnio <lepnio@gmail.com>
2014-06-14 09:37:33 +0200

    add clear + style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/*global $ */
 'use strict';
 
 /**
@@ -12,11 +13,32 @@ angular.module('rabbitmqDemoFrontApp')
     var httpPolling = function () {
       $timeout(function () {
         $http({method: 'GET', url: 'http://localhost:8080/rabbitmq/users'}).
-          success(function (data) {
-            $scope.users = data;
+          success(function (users) {
+            users.forEach(function(user) {
+              user.sendColor = function(color) {
+                var data = { userName: user.name, color: color };
+                $http({
+                  method: 'POST',
+                  url: 'http://localhost:8080/rabbitmq/users/color/one',
+                  data    : $.param(data),
+                  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+              };
+              user.clear = function() {
+                var data = { userName: user.name };
+                $http({
+                  method: 'POST',
+                  url: 'http://localhost:8080/rabbitmq/users/clear/one',
+                  data    : $.param(data),
+                  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+              }
+            });
+            $scope.users = users;
           }).
           error(function () {
             $scope.error = 'Server is down';
+            $scope.users = undefined;
           });
         httpPolling();
       }, 2000);
